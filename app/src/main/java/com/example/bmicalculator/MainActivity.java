b8b5d7385e58765e3e62920b35b8d83bb75df279
Xuyuan Zhu <zhuxuyuan1999@hotmail.com>
2020-04-25 23:19:23 -0400

    BMI calculation

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
 
     EditText weightInput;
     EditText heightInput;
+    TextView bmiResult;
+    RadioButton metricButton;
+    RadioButton englishButton;
+    Button calculateButton;
+    Button adviceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +31,49 @@ public class MainActivity extends AppCompatActivity {
 
         weightInput = findViewById(R.id.weightInput);
         heightInput = findViewById(R.id.heightInput);
+        bmiResult = findViewById(R.id.bmiResult);
+        metricButton = findViewById(R.id.metricButton);
+        englishButton = findViewById(R.id.englishButton);
+        calculateButton = findViewById(R.id.calculateButton);
+        adviceButton = findViewById(R.id.adviceButton);
+
+        return;
 
     }
 
     public void changeHintMetric(View view) {
         weightInput.setHint(hintWeightMetric);
         heightInput.setHint(hintHeightMetric);
+        return;
     }
 
     public void changeHintEnglish(View view) {
         weightInput.setHint(hintWeightEnglish);
         heightInput.setHint(hintHeightEnglish);
+        return;
+    }
+
+    public double getBmiMetric(double weight, double height){
+        double temp;
+        temp = weight/(height*height);
+        temp *= 100;
+        temp = Math.round(temp);
+        double result = temp/100;
+        return result;
+    }
+
+    public double getBmiEnglish(double weight, double height){
+        double temp;
+        temp = (weight*703)/(height*height);
+        temp *= 100;
+        temp = Math.round(temp);
+        double result = temp/100;
+        return result;
+    }
+
+    public void displayBMI(View view) {
+        String result = "hey";
+        bmiResult.setText(result);
+        return;
     }
 }
