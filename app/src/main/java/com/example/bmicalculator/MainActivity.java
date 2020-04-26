40805a4b6644f1397792a1e1de0d5b41f2fb1e6f
Xuyuan Zhu <zhuxuyuan1999@hotmail.com>
2020-04-25 23:34:29 -0400

    displayBMI()

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -11,11 +11,13 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // list out and initialize all the constants and strings
     String hintWeightMetric = "Enter weight in kilograms";
     String hintHeightMetric = "Enter height in meters";
     String hintWeightEnglish = "Enter weight in pounds";
     String hintHeightEnglish = "Enter height in inches";
 
+    // list out all components such as buttons and text inputs in the MainActivity
     EditText weightInput;
     EditText heightInput;
     TextView bmiResult;
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // find corresponding variable for each component
         weightInput = findViewById(R.id.weightInput);
         heightInput = findViewById(R.id.heightInput);
         bmiResult = findViewById(R.id.bmiResult);
@@ -41,18 +44,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // change hint of input boxes when chose metric
     public void changeHintMetric(View view) {
         weightInput.setHint(hintWeightMetric);
         heightInput.setHint(hintHeightMetric);
         return;
     }
 
+    // change hint of input boxes when chose english
     public void changeHintEnglish(View view) {
         weightInput.setHint(hintWeightEnglish);
         heightInput.setHint(hintHeightEnglish);
         return;
     }
 
+    // calculate bmi in metric units, returns a double
     public double getBmiMetric(double weight, double height){
         double temp;
         temp = weight/(height*height);
@@ -62,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
         return result;
     }
 
+    // calculate bmi in english units, returns a double
     public double getBmiEnglish(double weight, double height){
         double temp;
         temp = (weight*703)/(height*height);
@@ -71,8 +78,23 @@ public class MainActivity extends AppCompatActivity {
         return result;
     }
 
+    // display BMI value on screen when calculate button is clicked
     public void displayBMI(View view) {
-        String result = "hey";
+
+        double weight = Double.parseDouble(weightInput.getText().toString());
+        double height = Double.parseDouble(heightInput.getText().toString());
+
+        double bmi;
+        if ( metricButton.isChecked() ){
+            bmi = getBmiMetric(weight, height);
+        } else if ( englishButton.isChecked() ){
+            bmi = getBmiEnglish(weight, height);
+        } else {
+            // *display error message here*
+            return;
+        }
+
+        String result = String.format("%.2f", bmi);
         bmiResult.setText(result);
         return;
     }
